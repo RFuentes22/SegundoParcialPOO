9c43b4dc9c4c39e82371fbac6a9b09c127c8c5e2
R.Fuentes <R.Fuentes@RFuentes-PC>
2018-06-02 16:35:17 -0600

    Se crearon los paquetes y clases a utilizar

---
+++ src/robertofuentes/s/world/vehiculos/vehiculo3.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robertofuentes.s.world.vehiculos;
+
+/**
+ *
+ * @author R.Fuentes
+ */
+public class vehiculo3 {
+    
+}
