07fb5d62930e9c99ef460962dc9dca6d3c16af8e
R.Fuentes <R.Fuentes@RFuentes-PC>
2018-06-05 10:22:56 -0600

    Se mandan las unidades de las razas

--- src/robertofuentes/s/world/FactoryProducer.java
+++ src/robertofuentes/s/world/FactoryProducer.java
@@ -5,10 +5,23 @@
  */
 package robertofuentes.s.world;
 
+import robertofuentes.s.world.FactoryElfos.FactoryElfos;
+import robertofuentes.s.world.FactoryOrcos.FactoryOrcos;
+import robertofuentes.s.world.FactoryZombies.FactoryZombies;
+
 /**
  *
  * @author R.Fuentes
  */
 public class FactoryProducer {
-    
-}
+      public static AbstractFactory getFactory(String type) {
+      switch (type){
+          case "elfos":
+              return new FactoryElfos();
+          case "orcos":
+              return new FactoryOrcos();
+          case "zombies":
+                  return new FactoryZombies();
+      }
+      return null;
+}}
