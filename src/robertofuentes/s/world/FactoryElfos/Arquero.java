3ac25429d8b3b27626eaddf7ac754a6fdd8a33b7
R.Fuentes <R.Fuentes@RFuentes-PC>
2018-06-05 10:19:32 -0600

    Se crearon el metodo de defender

---
+++ src/robertofuentes/s/world/FactoryElfos/Arquero.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robertofuentes.s.world.FactoryElfos;
+
+/**
+ *
+ * @author R.Fuentes
+ */
+public class Arquero implements Elfos{
+    @Override
+    public void ataque() {
+        System.out.println("Arquero ataca");
+    }
+    
+     public void defiende() {
+        System.out.println("Arquero defiende");
+    }
+    
+}
