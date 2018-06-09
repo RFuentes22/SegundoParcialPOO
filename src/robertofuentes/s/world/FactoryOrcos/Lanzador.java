069e2dc99b0f3528d32efeb034a0321e240da922
R.Fuentes <R.Fuentes@192.168.0.10>
2018-06-09 12:40:37 -0600

    Se ha agregado Lanzador nuevamente

---
+++ src/robertofuentes/s/world/FactoryOrcos/Lanzador.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robertofuentes.s.world.FactoryOrcos;
+
+/**
+ *
+ * @author R.Fuentes
+ */
+public class Lanzador implements Orcos{
+ 
+     @Override
+    public void ataque() {
+        System.out.println("Lanzador ataca");
+    }
+    @Override 
+    public void defiende() {
+        System.out.println("Lanzador defiende");
+    }
+    
+    private int vida = 10;
+    private int cantLanzador = 1;
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getCantLanzador() {
+        return cantLanzador;
+    }
+
+    public void setCantLanzador(int cantLanzador) {
+        this.cantLanzador = cantLanzador;
+    }
+    
+}
+
+
